6fdc2a14cba19a2f9d394044297518d0a28d99ff
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-28 18:19:47 +0900

    fsck.f2fs: Disable kernel checks by default
    This really isn't helping and slows down the boot unnecessarily when
    a new kernel is installed.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Adithya <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I75870a9c100fab2600a64a550896876a386b2582

--- lib/libf2fs.c
+++ lib/libf2fs.c
@@ -678,11 +678,9 @@ void f2fs_init_configuration(void)
 	c.blks_per_seg = DEFAULT_BLOCKS_PER_SEGMENT;
 	c.wanted_total_sectors = -1;
 	c.wanted_sector_size = -1;
+	c.no_kernel_check = 1;
 #ifndef WITH_ANDROID
 	c.preserve_limits = 1;
-	c.no_kernel_check = 1;
-#else
-	c.no_kernel_check = 0;
 #endif
 
 	for (i = 0; i < MAX_DEVICES; i++) {
